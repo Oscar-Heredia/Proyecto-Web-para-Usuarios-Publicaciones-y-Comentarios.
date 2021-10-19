d9c3c11bb2a80fb20d50925b8c5160b5b1d350b6
Oscar-Heredia <79115876+Oscar-Heredia@users.noreply.github.com>
2021-10-18 23:51:34 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,65 @@
+const btnAdd = document.getElementById("btnCargar");
+  btnAdd.addEventListener("click",()=>{
+   fetch('https://jsonplaceholder.typicode.com/users')
+   .then(response => response.json())
+   .then(json => {
+   let info = "";
+   for (let i=0;i<json.length;i++){
+     info += `<option value = '${json[i].id}'> ${json[i].name} </option>`
+   }
+   const datos = document.getElementById("users")
+   datos.innerHTML=info;
+  });
+});
+
+
+const menuUser = document.getElementById("users");
+  menuUser.addEventListener("change",()=>{
+  const divdatos = document.getElementById("posts") 
+  let id = document.getElementById("users").value
+  fetch('https://jsonplaceholder.typicode.com/posts?userId='+ id)
+  .then((response) => response.json())
+  .then((json) => {
+    let datos = '';
+    for (let i=0;i<json.length;i++){
+      let a=i+1;
+      datos += `<div>
+        <h1>Obra Núm: ${a}</h1>
+        <h2>Titulo de la obra: </h2>
+        <h3 class="colortitulos">${json[i].title}</h3>
+        <h2>Información de la obra:</h2>
+        <p class="colorinformacion">${json[i].body}</p>
+        <button type="button" class='botonesfijs' onclick="cargarComments(${json[i].id})" >ver comentarios</button>
+        <div id="comment${json[i].id}">
+        </div> 
+      </div>`
+    }
+    divdatos.innerHTML = datos;
+  });
+});
+
+function cargarComments(postid){
+  // console.log(" clicked " , postid)
+  let comments = document.getElementById("comment" + postid);
+  fetch('https://jsonplaceholder.typicode.com/comments?postId=' + postid )
+       .then((response) => response.json())
+       .then((json)=> {
+         let comentario = "<button type='button' class='botonesfijs' onclick='eliminarComments("+ postid +")'>Quitar</button>";
+         for (let i=0; i<json.length;i++){
+           let a=i+1;
+           comentario += `<div class="comentarios" > <hr>
+             <h3 class="colortituloscomentarios">Comentario Núm: ${a}</h3>
+             <h3> Nombre:  ${json[i].name} </h3> 
+             <h4> Email:  ${json[i].email} </h4>
+             <p class="colorcomentarios"> Body:  ${json[i].body} </p>
+          </div>`
+       } 
+       comments.innerHTML = comentario;
+     });
+};
+
+
+function eliminarComments (postid){
+  let quitar = document.getElementById("comment" +postid);
+  quitar.innerHTML = "";
+};
